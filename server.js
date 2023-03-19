78248dae7d1cf198205c96194f15744a628c0e56
Giorgio Bonan <67089514+gigio27@users.noreply.github.com>
2023-03-19 21:51:58 +0200

    auth middleware

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const morgan = require('morgan')
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
+const {checkUser, requireAuth} = require('./middleware/auth.middleware')
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended : true }))
+app.use(cookieParser());
 app.use(morgan('dev'));
+
+// jwt to verify every request 
+app.get('*', checkUser);
+app.get('/jwtid', requireAuth, (req, res) => {
+    res.status(200).send(res.locals.user._id)
+})
 
 // routes
 app.use('/api/user', userRoutes);
-
-
 
 // server 
 app.listen(process.env.PORT, () => {
