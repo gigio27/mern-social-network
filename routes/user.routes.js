c1a45bcd6962cfbfbcf8997f2831e722120d9404
Giorgio Bonan <67089514+gigio27@users.noreply.github.com>
2023-03-19 10:13:03 +0200

    get all users

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
-const authController = require('../controllers/auth.controller.js');
+const authController = require('../controllers/auth.controller');
+const userController = require('../controllers/user.controller')
 
+// auth
 router.post("/register", authController.signUp);
 
+
+// user database
+router.get('/', userController.getAllUsers)
 
 module.exports = router;
