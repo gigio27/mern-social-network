77c38f45aada8b68b987d9bec3eebe76d4849ba1
Giorgio Bonan <67089514+gigio27@users.noreply.github.com>
2023-03-20 11:55:52 +0200

    unlike posts

--- controllers/post.controller.js
+++ controllers/post.controller.js
@@ -79,5 +79,23 @@ module.exports.likePost = async (req, res) => {
 }
 
 module.exports.unlikePost = async (req, res) => {
+    if (!ObjectID.isValid(req.params.id)) return res.status(400).send("ID unknown : " + req.params.id);
+    
+    try {
+        const post = await PostModel.findById(req.params.id);
+
+        if (!post) return res.status(404).send("Post not found");
+
+    
+
+        await post.updateOne({ $pull: { likers: req.body.id } });
+        res.status(200).json({ message: "The post has been unliked" });
+
+        await UserModel.findByIdAndUpdate(req.body.id, { $pull: { likes: req.params.id } });
+
+    } catch (err) {
+        console.log("Like Error : " + err);
+        return res.status(500).json({ message: err });
+    }
 
 }
